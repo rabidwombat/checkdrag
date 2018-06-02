8f2ad155931e5ae7b8667a79e0f3b6d977187420
Piercy <stephen.piercy@finra.org>
2018-06-01 20:35:14 -0400

    updates for mvp

--- checkdrag.js
+++ checkdrag.js
@@ -1,28 +1,20 @@
-var checkdrag = function(checkboxes) {
-  this.isDragging = false;
-  this.dragSetting;
-  this.clickBox = function(e) {
-    e.preventDefault();
-    return false;
-  };
-  this.mousedownBox = function(e) {
-    e.target.checked = !e.target.checked;
-    this.isDragging = true;
-    this.dragSetting = e.target.checked;
-  };
-  this.mouseoverBox = function(e) {
-    if (this.isDragging) {
-      e.target.checked = this.dragSetting;
-    }
-  };
-  this.mouseupDoc = function(e) {
-    this.isDragging = false;
-  };
+let checkdrag = function(checkboxes, options = {}) {
+  let intObj = {
+    isDragging: false,
+    dragSetting: undefined,
+    clickBox: e => e.preventDefault(),
+    mouseDownBox: e => {
+      this.dragSetting = e.target.checked = !e.target.checked;
+      this.isDragging = true;
+    },
+    mouseOverBox: e => {if (this.isDragging) e.target.checked = this.dragSetting},
+    mouseUpDoc: e => this.isDragging = false
+  }
 
   checkboxes.forEach(function(checkbox) {
-    checkbox.addEventListener('click', this.clickBox.bind(this));
-    checkbox.addEventListener('mousedown', this.mousedownBox.bind(this));
-    checkbox.addEventListener('mouseover', this.mouseoverBox.bind(this));
-    document.addEventListener('mouseup', this.mouseupDoc.bind(this));
+    checkbox.addEventListener('click', intObj.clickBox.bind(intObj));
+    checkbox.addEventListener('mousedown', intObj.mouseDownBox.bind(intObj));
+    checkbox.addEventListener('mouseover', intObj.mouseOverBox.bind(intObj));
+    document.addEventListener('mouseup', intObj.mouseUpDoc.bind(intObj));
   });
 };
